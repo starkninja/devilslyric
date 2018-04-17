656d673df7f3b6a1d4daabcac76079150a6654a5
Justin Acker <justin@justinacker.com>
2018-04-17 19:34:18 -0400

    add bio to users table; add bio and remove balance from users#show; change application#index to be more of a home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417225724) do
+ActiveRecord::Schema.define(version: 20180417232900) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180417225724) do
     t.integer  "balance"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "bio"
   end
 
 end
