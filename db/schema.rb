41a651e698202e9cba89eed97a0b7df584a4e19c
Justin Acker <justin@justinacker.com>
2018-04-17 18:58:23 -0400

    add note field to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416213027) do
+ActiveRecord::Schema.define(version: 20180417225724) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180416213027) do
     t.integer  "amount"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "note"
   end
 
   create_table "users", force: :cascade do |t|
