82a67c72f729d6751bad65946c18b3cac45c69d4
Justin Acker <justin@justinacker.com>
2018-04-17 20:26:20 -0400

    fix missing field param, change transaction create redirect

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,12 +13,25 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to @transaction, notice: 'Bonecoin sent.' }
-        format.json { render action: 'show', status: :created, location: @transaction }
+        format.html { redirect_to(transactions_url) }
+        format.json { render action: 'index', status: :created }
+        # redirect_to 'index'
       else
         format.html { render action: 'new'}
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  # def set_ingredient
+  #   @ingredient = Ingredient.find(params[:id])
+  # end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def transaction_params
+    params.require(:transaction).permit(:user_id, :recipient_id, :note, :amount)
   end
 
 end
