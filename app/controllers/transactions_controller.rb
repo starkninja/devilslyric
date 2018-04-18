539dd4bc87ba486ac93cec35484bb280f0e35ce9
Justin Acker <justin@justinacker.com>
2018-04-17 20:13:06 -0400

    first shot at transactions#new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,4 +4,21 @@ class TransactionsController < ApplicationController
     @transactions = Transaction.all.order('created_at DESC')
   end
 
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    respond_to do |format|
+      if @transaction.save
+        format.html { redirect_to @transaction, notice: 'Bonecoin sent.' }
+        format.json { render action: 'show', status: :created, location: @transaction }
+      else
+        format.html { render action: 'new'}
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+  end
+
 end
