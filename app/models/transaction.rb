caead687ccadd6789e0681662a3ddc029dd3a0a8
Justin Acker <justin@justinacker.com>
2018-04-19 18:30:11 -0400

    skeleton of transaction.pay

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,10 +6,31 @@ class Transaction < ActiveRecord::Base
     User.find(self.recipient_id)
   end
 
+
   def pay
     # check if balance can cover the transaction
     # if so, complete the transaction and return success
     # if not, don't do it and return an error
+    receiver = self.recipient
+    giver = self.user
+
+
+    if giver.balance >= self.amount
+
+      giver.balance -= self.amount
+      receiver.balance += receiver.amount
+      giver.save
+      receiver.save
+
+      return true
+
+    else
+
+      return false
+        #this is fine for now but
+        #how best to send other errors?
+        #http://api.rubyonrails.org/classes/ActiveModel/Errors.html
+
   end
 
 end
