d286c63ef1cbf8051131d08f4f39977817e4d7df
Justin Acker <justin@justinacker.com>
2018-04-26 15:30:22 -0400

    add generic session helpers to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
 
   def index
     @user = User.find(1)
+  end
+
+  def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    current_user.id != nil
+  end
+
+  def require_logged_in
+    return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
   end
 
 end
