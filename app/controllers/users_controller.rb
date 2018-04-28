d22deda55748ae6925dc688b1ba76219d762827b
Justin Acker <justin@justinacker.com>
2018-04-28 03:34:39 -0400

    fixed users#update call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation, :bio)
   end
 
 
